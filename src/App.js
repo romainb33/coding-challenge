f3fa65b24e2feb91c4317eba58ce5f78133d78dc
Romain B <romain.bnd@gmail.com>
2021-04-05 16:20:22 +0200

    refactor the code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import {Switch, Route, NavLink} from 'react-router-dom'
-import './App.css';
-import Temperature from './Components/Temperature';
-import CustomizeImage from './Components/CustomizeImage';
+import { Switch, Route, NavLink } from "react-router-dom";
+import "./App.css";
+import Temperature from "./Components/Temperature";
+import CustomizeImage from "./Components/CustomizeImage";
 
 function App() {
   return (
     <div className="App">
-
       <nav>
-        <NavLink to="/" >Home</NavLink>
-        <NavLink to="/temperature" >Temperature</NavLink>
-        <NavLink to="/customize-image" >Customize Image</NavLink>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/temperature">Temperature</NavLink>
+        <NavLink to="/customize-image">Customize Image</NavLink>
       </nav>
 
       <Switch>
         <Route exact path="/" />
         <Route exact path="/temperature" component={Temperature} />
-        <Route exact path="/customize-image" component={CustomizeImage}/>
+        <Route exact path="/customize-image" component={CustomizeImage} />
       </Switch>
-
     </div>
   );
 }
